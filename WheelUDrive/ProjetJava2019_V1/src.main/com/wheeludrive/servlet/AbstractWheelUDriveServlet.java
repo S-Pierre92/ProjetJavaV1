521e148a04f52705c9c7ca20c7ab5d48e8929250
snkizamacumu <snkizamacumu@ITEM-S33964.dhcp.brus.be.soprabanking>
2019-06-11 23:14:06 +0200

    Modification Abstract

--- WheelUDrive/ProjetJava2019_V1/src.main/com/wheeludrive/servlet/AbstractWheelUDriveServlet.java
+++ WheelUDrive/ProjetJava2019_V1/src.main/com/wheeludrive/servlet/AbstractWheelUDriveServlet.java
@@ -22,7 +22,7 @@ public abstract class AbstractWheelUDriveServlet extends HttpServlet {
 			"										</div>\r\n" + 
 			"									</a>\r\n" + 
 			"									<div class=\"dropdown-menu dropdown-menu-right px-4 py-3\">\r\n" + 
-			"										<form class=\"\" action=\"wheeludrive\" method=\"post\">\r\n" + 
+			"										<form class=\"\" action=\"${pageContext.request.contextPath}/wheeludrive\" method=\"post\">\r\n" + 
 			"											<div class=\"form-group\">\r\n" + 
 			"												<label>Email</label>\r\n" + 
 			"												<input name=\"emailConnexion\" type=\"email\" class=\"form-control\" placeholder=\"email@exemple.com\" required>\r\n" + 
@@ -39,5 +39,39 @@ public abstract class AbstractWheelUDriveServlet extends HttpServlet {
 			"									</div> <!--  dropdown-menu .// -->\r\n" + 
 			"								</div>  <!-- widget-header .// -->\r\n" + 
 			"							</div> <!-- widgets-wrap.// -->";
+	
+	
+	protected String getHtmlNotLoggedContext(String context) {
+		
+		return "<div class=\"widgets-wrap float-right\" id=\"connectForm\">\r\n" + 
+				"								<div class=\"widget-header dropdown\" >\r\n" + 
+				"									<a href=\"#\" data-toggle=\"dropdown\" data-offset=\"20,10\">\r\n" + 
+				"										<div class=\"icontext\">\r\n" + 
+				"											<div class=\"icon-wrap\"><i class=\"icon-sm round border fa fa-user\"></i></div>\r\n" + 
+				"											<div class=\"text-wrap\">\r\n" + 
+				"												<small style=\"color:#000;\">Se connecter | S'incrire</small>\r\n" + 
+				"											</div>\r\n" + 
+				"										</div>\r\n" + 
+				"									</a>\r\n" + 
+				"									<div class=\"dropdown-menu dropdown-menu-right px-4 py-3\">\r\n" + 
+				"										<form class=\"\" action=\""+context + "/wheeludrive\" method=\"post\">\r\n" + 
+				"											<div class=\"form-group\">\r\n" + 
+				"												<label>Email</label>\r\n" + 
+				"												<input name=\"emailConnexion\" type=\"email\" class=\"form-control\" placeholder=\"email@exemple.com\" required>\r\n" + 
+				"											</div>\r\n" + 
+				"											<div class=\"form-group\">\r\n" + 
+				"												<label>Mot de passe</label>\r\n" + 
+				"												<input type=\"password\" name=\"pswdConnexion\" class=\"form-control\" placeholder=\"Mot de passe\" required>\r\n" + 
+				"											</div>\r\n" + 
+				"											<input type=\"submit\" class=\"btn btn-primary\" value=\"Se connecter\">\r\n" + 
+				"											</form>\r\n" + 
+				"											<hr class=\"dropdown-divider\">\r\n" + 
+				"											<a class=\"dropdown-item\"  data-toggle=\"modal\" data-target=\"#modal-sign\">S'inscrire</a>\r\n" + 
+				"											<a class=\"dropdown-item\"  data-toggle=\"modal\" data-target=\"#modal-pswd-lost\">Mot de passe oublié?</a>\r\n" + 
+				"									</div> <!--  dropdown-menu .// -->\r\n" + 
+				"								</div>  <!-- widget-header .// -->\r\n" + 
+				"							</div> <!-- widgets-wrap.// -->";
+		
+	}
 	
 }
--- WheelUDrive/ProjetJava2019_V1/src.main/com/wheeludrive/servlet/VehiculeServlet.java
+++ WheelUDrive/ProjetJava2019_V1/src.main/com/wheeludrive/servlet/VehiculeServlet.java
@@ -4,9 +4,9 @@ import java.io.IOException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.apache.log4j.Logger;
 
@@ -19,7 +19,7 @@ import com.wheeludrive.entity.manager.VoitureManager;
 import com.wheeludrive.exception.PropertyException;
 
 @WebServlet(urlPatterns = { "/wheeludrive/vehicule" })
-public class VehiculeServlet extends HttpServlet {
+public class VehiculeServlet extends AbstractWheelUDriveServlet {
 	
 
 	/**
@@ -33,10 +33,27 @@ public class VehiculeServlet extends HttpServlet {
 	public final String VUE = "/WEB-INF/wheeludrive/index.jsp";
 
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
 		try {
 			this.mock = VoitureManager.findVoiture(2);
 			request.setAttribute("page", "vehicule");
+			request.getContextPath();
+			HttpSession session = request.getSession();
+			if(null != session.getAttribute("isLogged")) {
+				
+				int isLogged = (int) session.getAttribute( "isLogged" );
+				if(isLogged==1) {
+				    request.setAttribute("navFormLog", HTML_LOGGED);
+				    log.info("isloggedget");
+				}else {
+				    request.setAttribute("navFormLog", this.getHtmlNotLoggedContext(request.getContextPath()));
+					  log.info("isnotloggedget");
+				}
+			}else {
+			    request.setAttribute("navFormLog", this.getHtmlNotLoggedContext(request.getContextPath()));
+
+			}
+
+			
 			UtilisateurBean userBean = UtilisateurBeanConverter.convert(mock.getUtilisateur());
 			request.setAttribute(VENDEUR, userBean);
 			VoitureBean bean = VoitureBeanConverter.convert(this.mock);
