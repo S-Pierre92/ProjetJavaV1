446ffe8a5a45d1cd8448bd8899b6621d063901b9
Antoine Lafaille <al@live.be>
2019-06-14 20:35:08 +0200

    Réparation

--- WheelUDrive/ProjetJava2019_V1/src.main/com/wheeludrive/servlet/VehiculesServlet.java
+++ WheelUDrive/ProjetJava2019_V1/src.main/com/wheeludrive/servlet/VehiculesServlet.java
@@ -2,32 +2,26 @@ package com.wheeludrive.servlet;
 
 import java.io.IOException;
 
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.log4j.Logger;
 
-@WebServlet(urlPatterns = { "/wheeludrive/voitures" })
-public class VehiculesServlet extends HttpServlet{
+
+@WebServlet(urlPatterns = { "/wheeludrive/vehicules" })
+public class VehiculesServlet extends AbstractWheelUDriveServlet{
 	
+	private final static Logger log = Logger.getLogger(VehiculesServlet.class);
 	public final String VUE = "/WEB-INF/wheeludrive/index.jsp";
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 	
-	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-		
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {		
 		request.setAttribute("page", "vehicules");
-
+		request = this.checkSession(request, log);
 		this.getServletContext().getRequestDispatcher(VUE).forward(request, response);
 	}
-	
-	
-
 }
+
