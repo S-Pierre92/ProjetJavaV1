f5662e6daa61131790464a92cb242d1b30750ef2
Azen <jd100989@gmail.com>
2019-06-14 20:45:41 +0200

    refactoring commande

--- WheelUDrive/ProjetJava2019_V1/src.main/com/wheeludrive/servlet/CommandeServlet.java
+++ WheelUDrive/ProjetJava2019_V1/src.main/com/wheeludrive/servlet/CommandeServlet.java
@@ -8,6 +8,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.apache.log4j.Logger;
 
@@ -23,69 +24,159 @@ import com.wheeludrive.entity.manager.UtilisateurManager;
 import com.wheeludrive.exception.PropertyException;
 
 @WebServlet(urlPatterns = { "/wheeludrive/commande" })
-public class CommandeServlet extends HttpServlet {
-	
-	
+public class CommandeServlet extends AbstractWheelUDriveServlet {
+
+
 	private static final long serialVersionUID = 1L;
+
 	private Commande commande = new Commande();
 	private Contrat contrat = new Contrat();
 	private Facture facture = new Facture();
+
 	private final static Logger log = Logger.getLogger(CommandeServlet.class);
-	private final String ID_UTILISATEUR = "id_utilisateur";
+
 	private final String ID_ANNONCE = "id_annonce";
 
-	public final String VUE = "/WEB-INF/wheeludrive/home.jsp";
+	public final String VUE = "/WEB-INF/wheeludrive/index.jsp";
 	
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.setAttribute("page", "commande");
-		try {
-			Annonce anc = AnnonceManager.findAnnonce(Integer.parseInt(request.getParameter(ID_ANNONCE)));
-			Utilisateur usr = UtilisateurManager.findUtilisateur(Integer.parseInt(request.getParameter(ID_UTILISATEUR)));
 
-			
-			float tva = usr.getAdressesUtilisateurs().get(0).getAdresse().getCodePostal().getPays().getTauxTVA();
-			
-			//s'il n'y a pas de tva, la prix htva est fixe
-			if(tva == 0) {
-				tva = 0.21f;
+		HttpSession session = request.getSession();
+		request.setAttribute("page", "home");
+		if (null != session.getAttribute("isLogged")) {
+
+			int isLogged = (int) session.getAttribute("isLogged");
+			if (isLogged == 1) {
+				request.setAttribute("navFormLog", HTML_LOGGED);
+
+				/* Genere les valeurs pour la commande
+				 * Genere les valeurs pour la facture
+				 * Desactive l'annonce du vendeur
+				 * Averti le client que son achat a ete valide
+				 */
+				try {
+
+				} catch (NumberFormatException e) {
+					log.debug(e.getMessage());
+				}
+
+				log.info("isloggedget");
+			} else {
+				request.setAttribute("navFormLog", HTML_NOTLOGGED);
+
+				log.info("isnotloggedget");
 			}
-			
-			// On rempli les differents champs afin de completer le contrat (detail commande)
-			this.contrat.setVoiture(anc.getVoiture());
-			this.contrat.setMontantHT(anc.getMontant());
-			this.contrat.setMontantTTC(anc.getMontant()*(1+tva));
-			this.contrat.setTypesContrat(ContratCommandeManager.findTypesContrat(1));
-			
 
-			// on remplis les differents champ de la commande
-			this.commande.setTvaCourante(tva);
-			this.commande.setDateCommande((new Date()));
-			this.commande.setMontantTotalHtva(anc.getMontant());
-			this.commande.setUtilisateur(usr);
-			
-			ContratCommandeManager.createCommande(this.commande);
-			
-			this.contrat.setCommande(this.commande);
-			
-			ContratCommandeManager.createContrat(this.contrat);
-			
-			//On retire l'annonce de la vente
-			anc.setActif(false);
-			AnnonceManager.updateAnnonce(anc);
-			
-			// on remplis le champ de la facture
-			this.facture.setDateFacture(new Date());
-			this.facture.setCommande(this.commande);
-			FactureManager.createFacture(this.facture);
-			
-			
-			//TO DO
-			//Envoyer un mail comme quoi son achat est validé a l'acheteur
-			//renvoyer vers la bonne vue.
-			this.getServletContext().getRequestDispatcher(VUE).forward(request, response);
-		} catch (PropertyException e) {
-			log.error("Probleme: ",e);
+		} else {
+			request.setAttribute("navFormLog", HTML_NOTLOGGED);
 		}
+
+		if (request.getParameter("logout") != null) {
+			request.setAttribute("page", "home");
+
+			request.setAttribute("navFormLog", HTML_NOTLOGGED);
+			session.invalidate();
+			this.getServletContext().getRequestDispatcher(VUE).forward(request, response);
+			return;
+		}
+		this.getServletContext().getRequestDispatcher(VUE).forward(request, response);
+	}
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		HttpSession session = request.getSession();
+		request.setAttribute("page", "home");
+		if (null != session.getAttribute("isLogged")) {
+
+			int isLogged = (int) session.getAttribute("isLogged");
+			if (isLogged == 1) {
+				request.setAttribute("navFormLog", HTML_LOGGED);
+
+				/* Genere les valeurs pour la commande
+				 * Genere les valeurs pour la facture
+				 * Desactive l'annonce du vendeur
+				 * Averti le client que son achat a ete valide
+				 */
+				try {
+					this.generateCommand(request);
+					this.generateFacture();
+
+					//TODO methode fantome a faire pour plus tard
+					this.sendMailToCustomer();
+
+				} catch (NumberFormatException | PropertyException e) {
+					log.debug(e.getMessage());
+				}
+
+				log.info("isloggedget");
+			} else {
+				request.setAttribute("navFormLog", HTML_NOTLOGGED);
+
+				log.info("isnotloggedget");
+			}
+
+		} else {
+			request.setAttribute("navFormLog", HTML_NOTLOGGED);
+		}
+
+		if (request.getParameter("logout") != null) {
+			request.setAttribute("page", "home");
+
+			request.setAttribute("navFormLog", HTML_NOTLOGGED);
+			session.invalidate();
+			this.getServletContext().getRequestDispatcher(VUE).forward(request, response);
+			return;
+		}
+		this.getServletContext().getRequestDispatcher(VUE).forward(request, response);
+	}
+
+	private void generateCommand(HttpServletRequest request) throws NumberFormatException, PropertyException {
+
+		Annonce anc = AnnonceManager.findAnnonce(Integer.parseInt(request.getParameter(ID_ANNONCE)));
+		Utilisateur usr = UtilisateurManager.findUtilisateur((int)request.getSession().getAttribute("userId"));
+
+		float tva = usr.getAdressesUtilisateurs().get(0).getAdresse().getCodePostal().getPays().getTauxTVA();
+
+		//s'il n'y a pas de tva, la prix htva est fixe
+		if(tva <= 0) {
+			tva = 0.21f;
+		}
+
+		// On rempli les differents champs afin de completer le contrat (detail commande)
+		this.contrat.setVoiture(anc.getVoiture());
+		this.contrat.setMontantHT(anc.getMontant());
+		this.contrat.setMontantTTC(anc.getMontant()*(1+tva));
+		this.contrat.setTypesContrat(ContratCommandeManager.findTypesContrat(1));
+
+
+		// on remplis les differents champ de la commande
+		this.commande.setTvaCourante(tva);
+		this.commande.setDateCommande((new Date()));
+		this.commande.setMontantTotalHtva(anc.getMontant());
+		this.commande.setUtilisateur(usr);
+
+		ContratCommandeManager.createCommande(this.commande);
+
+		this.contrat.setCommande(this.commande);
+
+		ContratCommandeManager.createContrat(this.contrat);
+
+		anc.setActif(false);
+		AnnonceManager.updateAnnonce(anc);
+	}
+
+	private void generateFacture() throws NumberFormatException, PropertyException {
+		// on remplis le champ de la facture
+		this.facture.setDateFacture(new Date());
+		this.facture.setCommande(this.commande);
+		FactureManager.createFacture(this.facture);
+	}
+
+	private void sendMailToCustomer() {
+
+		//TODO
+		//Envoyer un mail comme quoi son achat est validï¿½ a l'acheteur
+		//
 	}
 
 }
